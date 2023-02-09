8da29ce7a918fb30a8d38f44be9e44679021a01c
john.russell <john.russell@synthego.com>
2023-02-08 17:37:54 -0800

    Changed web-eligible criteria to be based on dim_webstore_product_sku table in analyst reporting schema.

--- webstore_products.sql
+++ webstore_products.sql
@@ -1,34 +1,40 @@
+use database da_prod_db;
+use schema analyst_reporting;
+
+create or replace view vw_buckaneer_orders as
+(
 with web_eligible_products as (
     select
         product_line
         , product_category
         , product_type
-        , sku_list_product_name                                                 as product_name
+        , sku_list_product_name as product_name
         , product_code
     from
         da_prod_db.analyst_reporting.dim_webstore_product_sku
     )
-, buckaneer_orders as (
+   , buckaneer_orders as (
     select
         odg.order_id
-        , upper(pp.family)                                                      as family
+        , upper(pp.family)                  as family
         , oi.source_system
         , oo.status
-        , oo.created                                                            as order_created
+        , oo.created                        as order_created
         , oo.in_process_start_date
         , oo.estimated_ship_date
         , oo.actual_ship_date
         , oi.product_id
-        , pp.name                                                               as product_name
-        , pp.netsuite_part_number                                               as sku
-        , oi.product_name                                                       as product_details
+        , pp.name                           as product_name
+        , wep.product_category
+        , pp.netsuite_part_number           as sku
+        , oi.product_name                   as product_details
         , oi.units_sold
-        , oi.units_sold * oi.unit_price_net                                     as units_x_net
+        , oi.units_sold * oi.unit_price_net as units_x_net
         , oi.unit_price_net
         , oi.unit_price_gross
         , oi.unit_price_list
         , oi.product_config
-        , upa.company_name                                                      as shipping_company_name
+        , upa.company_name                  as shipping_company_name
         , upa.first_name
         , upa.last_name
         , upa.notification_email
@@ -40,13 +46,15 @@ with web_eligible_products as (
         left join stitch.stitch_buckaneer_prod.product_product                  pp
             on oi.product_id = pp.id
             and lower(pp.name) not like '%shipping%' -- exclude shipping addon charges
+        left join web_eligible_products                                         wep
+            on pp.netsuite_part_number = wep.product_code
         left join stitch.stitch_buckaneer_prod.order_order                      oo
             on odg.order_id = oo.id
             and oo.status not in ('cart', 'canceled', 'declined', 'new')
         left join stitch.stitch_buckaneer_prod.userprofile_address              upa
             on oo.shipping_address_id = upa.id
     )
-, extract_product_info as (
+   , extract_product_info as (
     select
         bo.order_id
         , bo.family
@@ -58,6 +66,7 @@ with web_eligible_products as (
         , bo.actual_ship_date
         , bo.product_id
         , bo.product_name
+        , bo.product_category
         , bo.sku
         , bo.product_details
         , bo.units_sold
@@ -94,17 +103,19 @@ select
     , epi.estimated_ship_date
     , epi.actual_ship_date
     , epi.product_id
-    , case
-          when epi.product_id in (40, 44, 1008, 1009, 1010
-                                , 1011, 1183, 1187, 1216, 1331)
-              then 'Knockout Cell Pool'
-          when epi.product_id in (45, 1139, 1176, 1214, 1219)
-              then 'Knockout Cell Clone'
-          when epi.product_type_json = 'engineered_cell_libraries'
-              then 'Engineered Cell Libraries'
-          else 'Other'
-      end                                                                       as product_type
+--     , case
+--           when epi.product_id in (40, 44, 1008, 1009, 1010, 1011, 1183, 1187, 1216, 1331)
+--               then 'Knockout Cell Pool'
+--           when epi.product_id in (45, 1139, 1176, 1214, 1219)
+--               then 'Knockout Cell Clone'
+--           when epi.product_type_json = 'engineered_cell_libraries'
+--               or epi.product_name ilike '%library'
+--               then 'Engineered Cell Library'
+--           else 'Other'
+--       end                                              as product_type
     , epi.product_name
+    , epi.product_category
+    , epi.sku
     , epi.product_details
     , epi.units_sold
     , epi.units_x_net
@@ -127,16 +138,17 @@ select
     , epi.cell_population
     , epi.species
     , iff(epi.sku in (
-        select
-            product_code
-        from
-            web_eligible_products
-        )
+    select
+        product_code
+    from
+        web_eligible_products
+    )
     , true
-    , false)                                                                    as is_webstore_eligible
-    , iff(epi.source_system = 'webstore', True, False)                          as is_webstore_order
+    , false)                                           as is_webstore_eligible
+    , iff(epi.source_system = 'webstore', true, false) as is_webstore_order
 from
-    extract_product_info                                                        as epi
+    extract_product_info as epi
 where
-    epi.family not in ('edit_credit')
+    epi.family not in ('EDIT_CREDIT')
     and epi.estimated_ship_date is not null
+    )
